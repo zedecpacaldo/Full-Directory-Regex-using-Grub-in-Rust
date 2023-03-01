f55c909417689858f63b20d1173e9ed2a044c30f
SmurfNiZed <zipacaldo@up.edu.ph>
2023-03-01 23:49:28 +0800

    fp and imp complete, prints out absolute path in lexicographical order

--- bin/fp.rs
+++ bin/fp.rs
@@ -1,3 +1,30 @@
+use glob::glob;
+use regex::Regex;
+use std::{fs, env};
+
+
 fn main() {
-    println!("Hello, this is fp!");
+    let args: Vec<_> = env::args().collect();                                                              // arguments array
+
+    let source_files_glob = &args[1]; 
+
+    let re = Regex::new(&args[2]).unwrap();
+
+
+    let file_names: Vec<_> = glob(source_files_glob)
+    .expect("Failed to read glob pattern")
+    .filter_map(Result::ok)
+    .filter_map(|path| {
+        let contents = fs::read_to_string(&path);
+        if re.is_match(&contents.unwrap()) {
+            Some(path)
+        } else {
+            None
+        }
+    })
+    .map(|path| path.parent().unwrap().to_string_lossy().into_owned() + "\\" + &path.file_name().unwrap().to_string_lossy().into_owned())
+    .collect();
+
+    println!("{}", file_names.join("\n"));
+
 }
--- bin/imp.rs
+++ bin/imp.rs
@@ -1,3 +1,32 @@
+use glob::glob;
+use regex::Regex;
+use std::{fs, env};
+
+
 fn main() {
-    println!("Hello, this is imp!");
+    let args: Vec<_> = env::args().collect();                                                              // arguments array
+
+    let mut array: Vec<String> = vec![String::new()];
+
+    let source_files_glob = &args[1]; 
+
+    let re = Regex::new(&args[2]).unwrap();
+    array.pop();
+
+    for entry in glob(source_files_glob).expect("Invalid glob path") {
+        match entry {
+            Ok(path) => {
+                let contents = fs::read_to_string(&path);
+                if re.is_match(&contents.unwrap()) {
+                    array.push(path.parent().unwrap().to_str().unwrap().to_string() + "\\" + path.file_name().unwrap().to_str().unwrap());
+                }
+            }
+            Err(_e) => {
+                println!("Error");
+            }
+        }
+    }
+
+    println!("{}", array.join("\n"));
+
 }
