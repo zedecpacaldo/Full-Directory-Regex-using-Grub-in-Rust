199127d4f168a51412152134eca79166784a01ed
SmurfNiZed <zipacaldo@up.edu.ph>
2023-03-04 20:18:51 +0800

    Final

--- bin/fp.rs
+++ bin/fp.rs
@@ -2,29 +2,41 @@ use glob::glob;
 use regex::Regex;
 use std::{fs, env};
 
-
 fn main() {
     let args: Vec<_> = env::args().collect();                                                              // arguments array
-
-    let source_files_glob = &args[1]; 
-
-    let re = Regex::new(&args[2]).unwrap();
-
-
-    let file_names: Vec<_> = glob(source_files_glob)
-    .expect("Failed to read glob pattern")
-    .filter_map(Result::ok)
-    .filter_map(|path| {
-        let contents = fs::read_to_string(&path);
-        if re.is_match(&contents.unwrap()) {
-            Some(path)
-        } else {
-            None
+    let glob_pattern = format!("{}", args[1]); 
+    match glob(&glob_pattern) {
+        Ok(_paths) => {
+            let reg_pattern = format!("{}", args[2]);
+            let re = match Regex::new(&reg_pattern) {
+                Ok(regex) => regex,
+                Err(_error) => {
+                    eprintln!("Invalid regex string");
+                    return
+                }
+            };
+            
+            let file_names: Vec<_> = glob(&glob_pattern)
+            .expect("Invalid glob pattern")
+            .filter_map(Result::ok)
+            .filter_map(|path| {
+                let contents = fs::read_to_string(&path);
+                if re.is_match(&contents.unwrap()) {
+                    Some(path)
+                } else {
+                    None
+                }
+            })
+            .map(|path| path.parent().unwrap().to_string_lossy().into_owned() + "\\" + &path.file_name().unwrap().to_string_lossy().into_owned())
+            .collect();
+        
+            println!("{}", file_names.join("\n"));
         }
-    })
-    .map(|path| path.parent().unwrap().to_string_lossy().into_owned() + "\\" + &path.file_name().unwrap().to_string_lossy().into_owned())
-    .collect();
+        Err(_err) => {
+            eprintln!("Invalid glob pattern");
+            return
+        }
+    }
 
-    println!("{}", file_names.join("\n"));
-
+    
 }
--- bin/imp.rs
+++ bin/imp.rs
@@ -1,32 +1,43 @@
-use glob::glob;
+use glob::{glob};
 use regex::Regex;
 use std::{fs, env};
 
 
 fn main() {
-    let args: Vec<_> = env::args().collect();                                                              // arguments array
-
+    // arguments array
+    let args: Vec<_> = env::args().collect();                                                                                           
     let mut array: Vec<String> = vec![String::new()];
-
-    let source_files_glob = &args[1]; 
-
-    let re = Regex::new(&args[2]).unwrap();
-    array.pop();
-
-    for entry in glob(source_files_glob).expect("Invalid glob path") {
-        match entry {
-            Ok(path) => {
-                let contents = fs::read_to_string(&path);
-                if re.is_match(&contents.unwrap()) {
-                    array.push(path.parent().unwrap().to_str().unwrap().to_string() + "\\" + path.file_name().unwrap().to_str().unwrap());
+    let glob_pattern = format!("{}", args[1]); 
+    match glob(&glob_pattern) {
+        Ok(_paths) => {
+            let reg_pattern = format!("{}", args[2]);
+            let re = match Regex::new(&reg_pattern) {
+                Ok(regex) => regex,
+                Err(_error) => {
+                    eprintln!("Invalid regex string");
+                    return
+                }
+            };
+            array.pop();
+            for entry in glob(&glob_pattern).unwrap() {
+                match entry {
+                    Ok(path) => {
+                        let contents = fs::read_to_string(&path);
+                        if re.is_match(&contents.unwrap()) {
+                            array.push(path.parent().unwrap().to_str().unwrap().to_string() + "\\" + path.file_name().unwrap().to_str().unwrap());
+                        }
+                    }
+                    Err(_e) => {
+                        println!("Invalid file");
+                    }
                 }
             }
-            Err(_e) => {
-                println!("Error");
-            }
+        
+            println!("{}", array.join("\n"));
+        }
+        Err(_err) => {
+            eprintln!("Invalid glob pattern");
+            return
         }
     }
-
-    println!("{}", array.join("\n"));
-
 }
